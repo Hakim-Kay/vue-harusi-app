5c32792ee1c6bd283fcc21fc2842e1bacd415092
Makini Hakim Kayira <95077113+Hakim-Kay@users.noreply.github.com>
2024-10-04 08:33:07 +0100

    Issue #18 (#2)
    * Implement Lighthouse in CI Pipeline
    
    * Rename lighthouserc.js -> lighthouserc.cjs
    
    * Change CI pipeline to reflect LHCI docs
    
    * Change staticDistDir to use '/dist' value
    
    * Update lighthouse-ci.yaml file
    
    * Delete lighthouserc.cjs file
    
    * Update lighthouse-ci file
    
    * Update pwa-audit, packag.json, and delete starter components
    
    * Update App.vue file
    
    * Update lighthouserc.json file
    
    * Update pwa-audit.yaml file
    
    * Update pwa-audit.yaml file
    
    * Introduce PWA, Configured Tailwindcss, Update vite.config file, Create example component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
+import Header1 from "./components/header1.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="page-wrapper">
+    <main>
+      <section class="padding-section-large">
+        <div class="padding-global">
+        <div class="container-medium">
+          <header1 />
+        </div>
+        </div>
+      </section>
+    </main>
+    <div>
+      <p class="heading-style-h1 text-align-center teal text-amber-300 container ">Hello Fahmy, how are you?</p>
+    </div>
+    <div class="container-large">
+      <div class="grid-cols-2"></div>
+    </div>
   </div>
 </template>
 
